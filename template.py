a65c03cbb2dc86ca6ac2445618c75b1bef52f703
ujj-kumar <batman.c731@gmail.com>
2024-06-23 01:06:24 +0530

    Folder Structure Defined

--- template.py
+++ template.py
@@ -9,7 +9,7 @@ list_of_files = [
     'src/components/data_ingestion.py',
     'src/components/data_transformation.py',
     'src/components/model_trainer.py',
-    'src/components/model_evaluation.py'
+    'src/components/model_evaluation.py',
     'src/pipeline/__init__.py',
     'src/pipeline/training_pipeline.py',
     'src/pipeline/prediction_pipeline.py',
